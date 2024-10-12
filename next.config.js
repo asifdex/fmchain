df58fe60d7255ba3107c27a4729cd27f27f15bc0
xashadulpo <xashadulasif@gmail.com>
2024-10-12 10:22:44 +0600

    next.config changes for deploy on vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `frame-ancestors 'self' http://localhost:* https://your-vercel-app.vercel.app https://secure-mobile.walletconnect.com https://secure-mobile.walletconnect.org`,
+          },
+        ],
+      },
+    ];
+  },
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/exp1.svg" />
+        <link rel="shortcut icon" href="fm.svg" />
        
       </Head>
       <body>
         <Main />
         <NextScript />
-        <title>Matar</title>
+        <title>fmcoin</title>
         <meta
           name="description"
           content=""
